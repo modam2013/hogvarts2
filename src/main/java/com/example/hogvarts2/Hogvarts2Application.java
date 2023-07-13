98681b814a73493579e4bed8e4626b397ab8adb1
Аня <grigorevna.antoninka@mail.ru>
2023-07-13 17:55:02 +0700

    Initial commit

---
+++ src/main/java/com/example/hogvarts2/Hogvarts2Application.java
@@ -0,0 +1,13 @@
+package com.example.hogvarts2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hogvarts2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hogvarts2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hogvarts2/Hogvarts2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hogvarts2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hogvarts2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
